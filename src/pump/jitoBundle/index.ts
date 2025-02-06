5329ea5c16b1dd701f1aac7467cc5cb8809550bc
solrose427 <rosefragrant183@gmail.com>
2025-02-06 00:58:23 -0600

    Update Jito bundle function for Solana pump.fun bundler codebase
    Update Jito bundle function for Solana pump.fun bundler codebase

--- src/pump/jitoBundle/index.ts
+++ src/pump/jitoBundle/index.ts
@@ -7,14 +7,14 @@ export const jitoBundle = async (transactions: VersionedTransaction[], payer: Ke
   console.log('Starting Jito Bundling... Tx counts:', transactions.length);
 
   const tipAccounts = [
-    '96gYZGLnJYVFmbjzopPSU6QiEV5fGqZNyN9nmNhvrZU5',
-    'HFqU5x63VTqvQss8hp11i4wVV8bD44PvwucfZ2bU7gRe',
-    'Cw8CFyM9FkoMi7K7Crf6HNQqf4uEMzpKw6QNghXLvLkY',
-    'ADaUMid9yfUytqMBgopwjb2DTLSokTSzL1zt6iGPaS49',
-    'DfXygSm4jCyNCybVYYK6DwvWqjKee8pbDmJGcLWNDXjh',
-    'ADuUkR4vqLUMWXxW9gh6D6L8pMSawimctcNZ5pGwDcEt',
-    'DttWaMuVvTiduZRnguLF7jNxTgiMBZ1hyAumKUiL2KRL',
-    '3AVi9Tg9Uo68tJfuvoKvqKNWKkC5wPdSSdeBnizKZ6jT',
+    'account1',
+    'account2',
+    'account3',
+    'account4',
+    'account5',
+    'account6',
+    'account7',
+    'account8',
   ];
   const jitoFeeWallet = new PublicKey(tipAccounts[Math.floor(tipAccounts.length * Math.random())])
 
